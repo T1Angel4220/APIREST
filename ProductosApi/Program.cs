f16f5b275c16b93e67516fb0cbebe6e7a1a10da5
Sebastián Ortiz <sortiz7841@uta.edu.ec>
2025-09-02 15:57:34 -0500

    Se creó la base del proyecto

---
+++ ProductosApi/Program.cs
@@ -0,0 +1,25 @@
+using Application.Interfaces;
+using Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Inyección de dependencias
+builder.Services.AddSingleton<IProductoRepository, InMemoryProductoRepository>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
